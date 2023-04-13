3928e9c720828d51b9cbe4a500e5f3b4a718583d
alanhakhyeonsong <songs4805@naver.com>
2023-04-14 00:41:41 +0900

    Feat: equals, hashcode 재정의

--- 오브젝트/Object/src/main/java/chapter02/Money.java
+++ 오브젝트/Object/src/main/java/chapter02/Money.java
@@ -1,6 +1,7 @@
 package chapter02;
 
 import java.math.BigDecimal;
+import java.util.Objects;
 
 public class Money {
     public static final Money ZERO = Money.wons(0);
@@ -37,5 +38,22 @@ public class Money {
 
     public boolean isGreaterThanOrEqual(Money other) {
         return amount.compareTo(other.amount) >= 0;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Money money = (Money) o;
+        return amount.compareTo(money.amount) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(amount);
     }
 }
