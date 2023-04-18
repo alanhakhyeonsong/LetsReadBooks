3a1ddb8d9c002734d76b744f7963c538353abc32
alanhakhyeonsong <songs4805@naver.com>
2023-04-18 14:54:05 +0900

    Refactor: reserve 메소드 개선

--- 오브젝트/Object/src/main/java/chapter04/ReservationAgency.java
+++ 오브젝트/Object/src/main/java/chapter04/ReservationAgency.java
@@ -2,43 +2,7 @@ package chapter04;
 
 public class ReservationAgency {
     public Reservation reserve(Screening screening, Customer customer, int audienceCount) {
-        Movie movie = screening.getMovie();
-
-        boolean discountable = false;
-        for (DiscountCondition condition : movie.getDiscountConditions()) {
-            if (condition.getType() == DiscountConditionType.PERIOD) {
-                discountable = screening.getWhenScreened().getDayOfWeek().equals(condition.getDayOfWeek())
-                        && condition.getStartTime().compareTo(screening.getWhenScreened().toLocalTime()) <= 0
-                        && condition.getEndTime().compareTo(screening.getWhenScreened().toLocalTime()) >= 0;
-            } else {
-                discountable = condition.getSequence() == screening.getSequence();
-            }
-
-            if (discountable) {
-                break;
-            }
-        }
-
-        Money fee;
-        if (discountable) {
-            Money discountAmount = Money.ZERO;
-            switch (movie.getMovieType()) {
-                case AMOUNT_DISCOUNT:
-                    discountAmount = movie.getDiscountAmount();
-                    break;
-                case PERCENT_DISCOUNT:
-                    discountAmount = movie.getFee().times(movie.getDiscountPercent());
-                    break;
-                case NONE_DISCOUNT:
-                    discountAmount = Money.ZERO;
-                    break;
-            }
-
-            fee = movie.getFee().minus(discountAmount);
-        } else {
-            fee = movie.getFee();
-        }
-
+        Money fee = screening.calculateFee(audienceCount);
         return new Reservation(customer, screening, fee, audienceCount);
     }
 }
